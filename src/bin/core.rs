eb207b371dedd752cf663f489f59266c8a7b69b8
Hisham Ismail <mhishami@gmail.com>
2018-09-21 04:03:27 +0800

    Added cmd line tool

--- src/bin/core.rs
+++ src/bin/core.rs
@@ -1,7 +1,19 @@
+#[macro_use] extern crate clap;
+use clap::App;
 
 use core;
 
 fn main() {
     let core = core::hello();
     println!("Hello, world!: {}", core);
+
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    if let Some(peers) = matches.value_of("peers") {
+        match peers {
+            _ => println!("Peers: {:?}", peers),
+        }
+    } else {
+        eprintln!("--peers <ports> not being used");
+    }
 }
